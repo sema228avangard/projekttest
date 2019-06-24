a0fad0782dad9927cf4170f46e5986a48ba5559b
sema <dima-semin20@yandex.ru>
2019-06-25 00:31:26 +0300

    proek1

---
+++ projekt1/script.js
@@ -0,0 +1,16 @@
+var money = prompt ('Введите бюджет на месяц');
+var time = prompt ('Введите дату в формате YYYY-MM-DD');
+
+var appData = {
+budget: money,
+timeData: time,
+expenses:{
+    a:b,
+},
+optionalExpenses:{},
+income:[],
+savings:false,
+};
+var a = prompt ('Введите обязательную статью расходов в этом месяце');
+var b = prompt ('Во сколь это обойдется');
+alert(money/30);

a33163cacdcb21b7c29a223823ea914429f1ad02
sema <dima-semin20@yandex.ru>
2019-06-29 13:21:19 +0300

    цикл

--- projekt1/script.js
+++ projekt1/script.js
@@ -5,12 +5,51 @@ var appData = {
 budget: money,
 timeData: time,
 expenses:{
-    a:b,
+    
 },
 optionalExpenses:{},
 income:[],
 savings:false,
 };
-var a = prompt ('Введите обязательную статью расходов в этом месяце');
-var b = prompt ('Во сколь это обойдется');
-alert(money/30);
+
+// for (let i = 0;i < 2;i++){
+    
+//     let a = prompt ('Введите обязательную статью расходов в этом месяце'),
+//          b = prompt ('Во сколь это обойдется');
+//   if (  (typeof(a))=== 'string' && (typeof(a)) !=null && (typeof(b)) !=null && a !="" && b !="" && a.length<50){
+//     console.log("done");
+//     appData.expenses[a]=b;
+//   }else{}
+// }
+let i=0
+// while( i<2){
+//     let a = prompt ('Введите обязательную статью расходов в этом месяце'),
+//          b = prompt ('Во сколь это обойдется');
+//   if (  (typeof(a))=== 'string' && (typeof(a)) !=null && (typeof(b)) !=null && a !="" && b !="" && a.length<50){
+//     console.log("done");
+//     appData.expenses[a]=b;
+//   }else{}
+//   i++;
+
+// }
+do{
+    let a = prompt ('Введите обязательную статью расходов в этом месяце'),
+    b = prompt ('Во сколь это обойдется');
+if (  (typeof(a))=== 'string' && (typeof(a)) !=null && (typeof(b)) !=null && a !="" && b !="" && a.length<50){
+console.log("done");
+appData.expenses[a]=b;
+}else{}
+}while (++i<2)
+
+appData.moneyPerDay = appData.budget /30
+alert("ЕЖЕДНЕВНЫЙ БЮДЖЕТ:" + appData.moneyPerDay);
+if (appData.moneyPerDay<100){
+    console.log("Бомж");
+}else if(appData.moneyPerDay>100 && appData.moneyPerDay<2000){
+console.log("Средний");
+} else if (appData.moneyPerDay>2000){
+    console.log('Красавчик');
+} else {
+    console.log('Ошибка');
+}
+
